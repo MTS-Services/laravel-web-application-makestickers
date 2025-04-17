11ab354dfdba91caf9414220d72e16856fc9923c
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-04-17 14:45:17 +0600

    update layouts

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title') {{ isset($title) ?? ' | ' }} {{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title') | {{ config('app.name', 'Laravel') }}</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 

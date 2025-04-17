a39dea2efdee0b1316b3dc8ad7b2915e9dc744a2
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-04-17 13:06:30 +0600

    utility, custom class and daisyui setup done

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,50 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                "Lato": ["Lato", 'sans-serif'],
+                "Montserrat": ["Montserrat", 'sans-serif'],
             },
+            fontSize: {
+                'heading-1': ['2.5rem', '3rem'],
+                'heading-2': ['2rem', '2.5rem'],
+                'heading-3': ['1.75rem', '2rem'],
+                'heading-4': ['1.5rem', '2rem'],
+                'heading-5': ['1.25rem', '1.75rem'],
+                'heading-6': ['1rem', '1.5rem'],
+
+                'font-extra-large': ['1.5rem', '2rem'],
+                'font-large': ['1.25rem', '1.75rem'],
+                'font-primary': ['1rem', '1.5rem'],
+                'font-small': ['0.875rem', '1.25rem'],
+                'font-tiny': ['0.75rem', '1rem'],
+            },
+
+            colors: {
+                'primary': '#54B5D9',
+                'primary-hover': '#0188bd',
+                'secondary': '#a4d233',
+                'secondary-hover': '#b2d952',
+                'tertiary': '#F02737',
+                'tertiary-hover': '#f03d45',
+
+                'orange': '#F2A900',
+                'orange-hover': '#f4b626',
+
+                'dark-blue': '#0C4058',
+                'white': '#FFFFFF',
+                'black': '#000000',
+                'light-gray': '#F6F6F8',
+                'dark': '#333333',
+                'gray': '#737373',
+            },
+
+            boxShadow: {
+                'btn-primary': 'inset 0 1px 0 hsla(0,0%,100%,.15),0 1px 1px rgba(0,0,0,.075)',
+                'btn-secondary': '0 0 0 0.25rem rgba(var(--bs-btn-focus-shadow-rgb),.5)',
+            }
         },
     },
-    plugins: [],
+    plugins: [
+        require('daisyui'),
+    ],
 };

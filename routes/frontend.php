3034c508f1f6230312b662cd4074d566b11821e0
Rezwan KhAn <rez01khan07@gmail.com>
2025-04-20 10:45:53 +0600

    contact-page-done

--- routes/frontend.php
+++ routes/frontend.php
@@ -6,4 +6,5 @@ use App\Http\Controllers\Frontend\FrontendController;
 Route::group(['as' => 'frontend.', 'middleware' => 'guest'], function () {
     Route::get('/' , [FrontendController::class, 'home'])->name('home');
     Route::get('/about', [FrontendController::class, 'about'])->name('about');
+    Route::get('/contact', [FrontendController::class, 'contact'])->name('contact');
 });

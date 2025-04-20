90500a322c3e8ca955feb721cfed6af074190ebf
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-04-20 14:49:24 +0600

    returns policy done

--- resources/views/frontend/pages/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('frontend.layouts.app', ['page_slug' => 'home'])
-@section('title', 'Home')
-
-@push('styles')
-@endpush
-
-@section('content')
-    <a href="{{ route('frontend.returns') }}"> Returns Policy</a>
-@endsection

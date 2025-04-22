ef92fc9fbc5fedbf25c449a48b2c9fb4e210832d
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-04-22 14:38:17 +0600

    add migration files

---
+++ app/Models/Faq.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\BaseModel;
+
+class Faq extends BaseModel
+{
+    //
+}
---
+++ app/Models/MaterialCategory.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\BaseModel;
+
+class MaterialCategory extends BaseModel
+{
+    //
+}

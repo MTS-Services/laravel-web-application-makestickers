bb93e5318be76b326998b9b1e0a756d270b5ac7b
Md Shariful Islam <devshariful99@gmail.com>
2025-05-01 16:44:54 +0600

    Update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,6 @@
 import './bootstrap';
+import "./jquery";
 import './sweetalert2.js'
+import "./datatable";
+import "./toastr";
+import "./select2";
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,10 +1,22 @@
 import 'bootstrap';
+window.bootstrap = bootstrap;
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
+
+window.showModal = function (modal_id) {
+    var myModal = new bootstrap.Modal(document.getElementById(modal_id));
+    myModal.show();
+};
+window.hideModal = function (modal_id) {
+    var myModal = new bootstrap.Modal(document.getElementById(modal_id));
+    myModal.hide();
+};
+
+
 
 import axios from 'axios';
 window.axios = axios;

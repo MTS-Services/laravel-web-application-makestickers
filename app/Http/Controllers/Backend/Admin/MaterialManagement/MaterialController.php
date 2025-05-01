d4010fc84bb95f9ddd94060a1a6a7344accc96b2
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-05-01 15:30:26 +0600

    Material and material_atribute  model, migration and controller file created

---
+++ app/Http/Controllers/Backend/Admin/MaterialManagement/MaterialController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Admin\MaterialManagement;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MaterialController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+        
+        // Define permissions for each method
+        $this->middleware('permission:admin-list', ['only' => ['index', 'show']]);
+        $this->middleware('permission:admin-create', ['only' => ['create', 'store']]);
+        $this->middleware('permission:admin-edit', ['only' => ['edit', 'update', 'status']]);
+        $this->middleware('permission:admin-delete', ['only' => ['destroy']]);
+        $this->middleware('permission:admin-trash', ['only' => ['trash', 'restore']]);
+        $this->middleware('permission:admin-restore', ['only' => ['restore']]);
+        $this->middleware('permission:admin-force-delete', ['only' => ['forceDelete']]);
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    public function status(string $id, string $status)
+    {
+        //
+    }
+
+    public function trash()
+    {
+        //
+    }
+    public function restore(string $id)
+    {
+        //
+    }
+    public function forceDelete(string $id)
+    {
+        //
+    }
+}
---
+++ database/migrations/2025_05_01_090606_create_materials_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Http\Traits\AuditColumnTraits;
+
+return new class extends Migration
+{
+    use SoftDeletes, AuditColumnTraits;
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('materials', function (Blueprint $table) {
+            $table->id();
+            $table->integer('sort_order');
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('icons')->nullable();
+            $table->string('price_modifier')->decimal(5, 2)->default(1.00);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $this->addAdminAuditColumns($table);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('materials');
+    }
+};

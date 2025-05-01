e88b5bdd5a85b10089d057f05a9c53f261f97ff1
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-05-01 10:22:04 +0600

    update

--- app/Http/Controllers/Backend/Admin/DashboardController.php
+++ app/Http/Controllers/Backend/Admin/DashboardController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Backend\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class DashbordController extends Controller
+class DashboardController extends Controller
 {
     public function __construct()
     {
